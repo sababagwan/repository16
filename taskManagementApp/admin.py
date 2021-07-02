bda153a8e81f7d10a2d6e4e7412ee1ed5526c4b5
unknown <sababagwan16@gmail.com>
2021-07-02 11:19:47 +0530

    All files done

---
+++ taskManagementApp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import TaskDb
+
+admin.site.register(TaskDb)
+
+# Register your models here.
---
+++ taskManagementApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+class TaskDb(models.Model):
+    task = models.CharField(max_length=30)
+    priority = models.CharField(max_length=30)
+    completed = models.BooleanField(default=False)
+    time_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "%s %s" %(self.task, self.completed)
